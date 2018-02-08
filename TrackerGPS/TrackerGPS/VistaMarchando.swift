87213edb9be7f7dc9ab08209ea3f005148d2ddea
Unknown <inaki.caballero@egibide.org>
2018-02-08 21:47:04 +0100

    Contador hecho

--- TrackerGPS/TrackerGPS/VistaMarchando.swift
+++ TrackerGPS/TrackerGPS/VistaMarchando.swift
@@ -9,23 +9,61 @@
 import UIKit
 
 class VistaMarchando: UIViewController {
+    
+    //tema timer contador => https://medium.com/ios-os-x-development/build-an-stopwatch-with-swift-3-0-c7040818a10f
     var contador = 0
-    var timer = Timer()
+    var tiempo = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-                
-        
+        runTimer()
         
     }
-
+    
+    //funcion para bucle contador, sume mas 1
+    func runTimer() {
+        tiempo = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(VistaMarchando.contadorSuma)), userInfo: nil, repeats: true)
+    }
+    //el contador en si mismo, cambiando el tiempoLabel
+    @objc func contadorSuma() {
+        contador += 1
+        tiempoLabel.text = pasarBonitoElTiempo(time: TimeInterval(contador))
+       //print(contador)
+    }
+    //dejarlo bonito
+    func pasarBonitoElTiempo(time:TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format: "%02i:%02i:%02i", hours, minutes, seconds)
+    }
+    
+    
+    @IBOutlet weak var tiempoLabel: UILabel!
+    
+    //cuando se pulse cambie de estado el propio boton y el timeLabel.
+    @IBOutlet weak var pausaNombre: UIButton!
+    var estadoBotonPausa: Int = 0
+    @IBAction func pausaBoton(_ sender: Any) {
+        
+        if estadoBotonPausa == 0 {
+            estadoBotonPausa = 1
+            //pausar timer
+            tiempo.invalidate()
+            pausaNombre.setTitle("Reanudar", for: UIControlState.normal)
+        }else{
+            estadoBotonPausa = 0
+            //reanudar timer
+            runTimer()
+            pausaNombre.setTitle("Pausar", for: UIControlState.normal)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
-    
-    @IBOutlet weak var tiempo: UILabel!
     
     
 
