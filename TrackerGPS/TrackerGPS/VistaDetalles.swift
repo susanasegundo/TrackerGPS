0c13b81da091b8cdd3ef6ff092b42e670dc1896f
Unknown <inaki.caballero@egibide.org>
2018-02-21 15:59:33 +0100

    VistaDetalles con EUREKA

--- TrackerGPS/TrackerGPS/VistaDetalles.swift
+++ TrackerGPS/TrackerGPS/VistaDetalles.swift
@@ -7,35 +7,92 @@
 //
 
 import UIKit
+import Eureka
 
-class VistaDetalles: UIViewController {
+class VistaDetalles: FormViewController {
 
     var recorrido: Recorrido = Recorrido()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //actualizar labels
-        //dar el formato separado de Fecha y Hora para poner en cada label
-        let fecha = recorrido.fechaInicio
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd-MM-yyyy"
-        var fechaString = dateFormatter.string(from: fecha)
-        dateFormatter.dateFormat = "HH:mm:ss"
-        var horaString = dateFormatter.string(from: fecha)
-        //escribir en cada una de ellas
-        fechaInicio.text = fechaString
-        horaInicio.text = horaString
         
-        let feFin = recorrido.fechaFin
-         fechaString = dateFormatter.string(from: feFin)
-         horaString = dateFormatter.string(from: feFin)
-        //escribir en cada una de ellas
-        fechaFin.text = fechaString
-        horaFin.text = horaString
+        //formato fecha inicio
+        let fechaInicio = recorrido.fechaInicio
+        let dateFormatterInicio = DateFormatter()
+        dateFormatterInicio.dateFormat = "dd-MM-yyyy"
+        let fechaInicioString = dateFormatterInicio.string(from: fechaInicio)
+        dateFormatterInicio.dateFormat = "HH:mm:ss"
+        let horaInicioString = dateFormatterInicio.string(from: fechaInicio)
         
-        tiempoTotal.text = String(recorrido.tiempoT)
-        actividad.text = recorrido.tipo
+        //formato fecha fin
+        let fechaFin = recorrido.fechaFin
+        let dateFormatterFin = DateFormatter()
+        dateFormatterFin.dateFormat = "dd-MM-yyyy"
+        let fechaFinString = dateFormatterFin.string(from: fechaFin)
+        dateFormatterFin.dateFormat = "HH:mm:ss"
+        let horaFinString = dateFormatterFin.string(from: fechaFin)
         
+        //formato tiempo total
+        let tiempo = pasarBonitoElTiempo(time: recorrido.tiempoT)
+        
+        
+        form +++ Section("Inicio")
+            <<< TextRow() {
+                $0.title = "Fecha"
+                $0.baseValue = fechaInicioString
+                $0.disabled = true
+                
+                }.cellUpdate() {cell, row in
+                    cell.titleLabel?.textColor = UIColor.orange
+            }
+            <<< TextRow(){
+                $0.title = "Hora"
+                $0.value = horaInicioString
+                $0.disabled = true
+                }.cellUpdate() {cell, row in
+                    cell.textLabel?.textColor = UIColor.orange
+            }
+            
+            +++ Section("Fin")
+            <<< TextRow() {
+                $0.title = "Fecha"
+                $0.baseValue = fechaFinString
+                $0.disabled = true
+                }.cellUpdate() {cell, row in
+                    cell.textLabel?.textColor = UIColor.orange
+            }
+            <<< TextRow(){
+                $0.title = "Hora"
+                $0.value = horaFinString
+                $0.disabled = true
+                }.cellUpdate() {cell, row in
+                    cell.textLabel?.textColor = UIColor.orange
+        }
+            +++ Section("Actividad")
+            <<< TextRow() {
+                $0.title = "Actividad realizada"
+                $0.baseValue = recorrido.tipo
+                $0.disabled = true
+                }.cellUpdate() {cell, row in
+                    cell.textLabel?.textColor = UIColor.orange
+            }
+        
+            +++ Section("Tiempo transcurrido")
+            <<< TextRow() {
+                $0.title = "Tiempo de recorrido"
+                $0.baseValue = tiempo
+                $0.disabled = true
+                }.cellUpdate() {cell, row in
+                    cell.textLabel?.textColor = UIColor.orange
+            }
+        
+    }
+    
+    func pasarBonitoElTiempo(time:TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format: "%02i:%02i:%02i", hours, minutes, seconds)
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,12 +100,8 @@ class VistaDetalles: UIViewController {
         
     }
     
-    @IBOutlet weak var fechaInicio: UILabel!
-    @IBOutlet weak var horaInicio: UILabel!
-    @IBOutlet weak var fechaFin: UILabel!
-    @IBOutlet weak var horaFin: UILabel!
-    @IBOutlet weak var tiempoTotal: UILabel!
-    @IBOutlet weak var actividad: UILabel!
+    
+   
     
     
 
